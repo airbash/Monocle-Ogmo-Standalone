eaa5068d3d23b94e5e3e8a47660b151af1c95305
airbash <airbash@gmail.com>
2011-05-05 22:55:49 -0400

    Initial commit

---
+++ OgmoProject/premake4.lua
@@ -0,0 +1,44 @@
+--premake4.lua
+--
+-- Ogmo Standalone project, linking to MonocleCore library
+-- Usage: "premake4 vs2010"
+--
+
+-- Required globals for helper script
+_MONOCLE_APP			= "Ogmo"
+_MONOCLE_APP_BASE		= os.getcwd()								--root directory of your project
+_MONOCLE_BASE			= os.getcwd().."/../MonocleLibrary"			--root directory for Monocle
+
+dofile( (_MONOCLE_BASE.."/premake4-helper.lua") )
+
+--
+-- Monocle Solution
+--
+solution (_MONOCLE_SOLUTION_NAME)
+	basedir( _BUILD_BASE )
+	configurations { "Debug", "Release" }
+	
+		-- Generic test app premake4 script
+	project (_MONOCLE_APP)
+		kind "ConsoleApp"
+		language "C++"
+		basedir	( _BUILD_BASE )
+		
+		monocle_os_defines()
+		monocle_os_includedirs()
+		monocle_os_links()
+
+		files { _MONOCLE_APP_BASE.."/**.h", _MONOCLE_APP_BASE.."/**.cpp" }
+		
+		configuration "Debug"
+			defines { "DEBUG" }
+			flags { "Symbols" }
+			monocle_build_targetdir( "debug" )
+			
+		configuration "Release"
+			defines { "NDEBUG" }
+			flags { "Optimize" } 
+			monocle_build_targetdir( "release" )
+
+	-- Monocle Core Library
+	monocle_project_corelib();
